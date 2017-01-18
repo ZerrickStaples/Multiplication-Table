dd705bfd466d57b4dd6c457b9cffd94f5ddb7cfe
Zerrick Staples <zerrick.staples@gmail.com>
2017-01-18 15:47:00 -0500

    first commit

---
+++ MultiplicationTable/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiplicationTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 9; i++)
+            {
+                Console.WriteLine("Multiples of " + i);
+                for (int j = 1; j <= 9; j++)
+                {
+                    Console.Write(j * i + " ");
+                    Console.Write("\t");
+                }
+
+                Console.WriteLine("");
+            }
+        }
+    }
+}
